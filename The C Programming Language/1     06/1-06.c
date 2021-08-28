98b543333429b3aa94f612d0021cc5b2c3c1ea86
KushagrJ <kushagr.j@gmail.com>
2021-08-28 11:11:36 +0530

    Update C

---
+++ The C Programming Language/1     06/1-06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+    printf("Enter a character: ");
+
+    printf("getchar != EOF is %d\n", getchar() != EOF);
+
+    return 0;
+
+}
---
+++ The C Programming Language/1     07/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+    printf("EOF is %d\n", EOF);
+
+    return 0;
+
+}
