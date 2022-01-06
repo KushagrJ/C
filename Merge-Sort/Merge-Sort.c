8dbdc296d1503194ec90eb3f6338d23a5a4bdd81
KushagrJ <kushagr.j@gmail.com>
2022-01-06 17:14:55 +0530

    Update C

--- Merge-Sort/Merge-Sort.c
+++ Merge-Sort/Merge-Sort.c
@@ -1,38 +1,35 @@
-// To understand the recursion, first prove that the
-// merge_sort() function sorts any random 1-element array.
-// Then, prove that it sorts any random 2-element array.
-// Continuing in this manner, you will notice that it sorts
-// any random n-element array.
-// For eg., when proving that the function sorts any random
-// 5-element array, you don't have to prove that it sorts its
-// left and right halves, as you have already proved that it
-// sorts any random array with less than 5 elements.
+// To understand the recursion, first prove that the merge_sort() function sorts
+// any random 1-element array. Then, prove that it sorts any random 2-element
+// array. Continuing in this manner, you will notice that it sorts any random
+// n-element array.
+// For eg., when proving that the function sorts any random 5-element array, you
+// don't have to prove that it sorts its left and right halves, as you have
+// already proved that it sorts any random array with less than 5 elements.
 
-// Study another implementation of Merge Sort where the left
-// & right arays are created inside the merge() function, and
-// the merging process gets done using a single loop, by
-// setting the extra last elements of left & right to
-// INT_MAX.
+// Study another implementation of Merge Sort where the left & right arays are
+// created inside the merge() function, and the merging process gets done using
+// a single loop, by setting the extra last elements of left & right to INT_MAX.
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // Stable sort.
-void merge_sort(int *, unsigned);
-void merge(int *, int *, int *, unsigned, unsigned);
-
+void merge_sort(int*, unsigned);
+void merge(int*, int*, int*, unsigned, unsigned);
 
 int main(void)
 {
-
     unsigned size;
     printf("Enter the no. of integers to be sorted: ");
     scanf("%u", &size);
 
-    int * arr = (int *) malloc(size*sizeof(int));
+    int* arr = malloc(size * sizeof (int));
     if (arr == NULL)
+    {
+        fprintf(stderr, "Unsuccessful allocation\n");
         exit(EXIT_FAILURE);
+    }
+
     printf("Enter %u integers: ", size);
     for (unsigned i = 0; i < size; i++)
         scanf("%d", &arr[i]);
@@ -41,32 +38,35 @@ int main(void)
 
     printf("\nSorted array: ");
     for (unsigned i = 0; i < size; i++)
-        printf("%d ", arr[i]);
+        printf("%d \n", arr[i]);
     printf("\n");
 
     free(arr);
 
     return EXIT_SUCCESS;
-
 }
 
-
-void merge_sort(int * arr, unsigned size)
+void merge_sort(int* arr, unsigned size)
 {
-
     if (size > 1)
     {
         unsigned leftSize = size / 2;
-        int * left = (int *) malloc(leftSize*sizeof(int));
+        int* left = malloc(leftSize * sizeof (int));
         if (left == NULL)
+        {
+            fprintf(stderr, "Unsuccessful allocation\n");
             exit(EXIT_FAILURE);
+        }
         for (unsigned i = 0; i < leftSize; i++)
             left[i] = arr[i];
 
         unsigned rightSize = size - leftSize;
-        int * right = (int *) malloc(rightSize*sizeof(int));
+        int* right = malloc(rightSize * sizeof (int));
         if (right == NULL)
+        {
+            fprintf(stderr, "Unsuccessful allocation\n");
             exit(EXIT_FAILURE);
+        }
         for (unsigned i = 0; i < rightSize; i++)
             right[i] = arr[i + leftSize];
 
@@ -77,14 +77,11 @@ void merge_sort(int * arr, unsigned size)
         free(left);
         free(right);
     }
-
 }
 
-
-void merge(int * arr, int * left, int * right,
-           unsigned leftSize, unsigned rightSize)
+void merge(int* arr, int* left, int* right, unsigned leftSize,
+           unsigned rightSize)
 {
-
     unsigned i = 0, j = 0, k = 0;
 
     while ((i < leftSize) && (j < rightSize))
@@ -100,7 +97,6 @@ void merge(int * arr, int * left, int * right,
 
     while (j < rightSize)
         arr[k++] = right[j++];
-
 }
 
 
@@ -109,52 +105,17 @@ void merge(int * arr, int * left, int * right,
 
 /* Alternative :-
 
-#include <stdio.h>
-#include <stdlib.h>
-
-
-// Stable sort.
-void merge_sort(int *, unsigned);
-void merge(int *, int *, int *, unsigned, unsigned);
-
-
-int main(void)
+void merge_sort(int* arr, unsigned size)
 {
-
-    unsigned size;
-    printf("Enter the no. of integers to be sorted: ");
-    scanf("%u", &size);
-
-    int * arr = (int *) malloc(size*sizeof(int));
-    if (arr == NULL)
-        exit(EXIT_FAILURE);
-    printf("Enter %u integers: ", size);
-    for (unsigned i = 0; i < size; i++)
-        scanf("%d", &arr[i]);
-
-    merge_sort(arr, size);
-
-    printf("\nSorted array: ");
-    for (unsigned i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-
-    free(arr);
-
-    return EXIT_SUCCESS;
-
-}
-
-
-void merge_sort(int * arr, unsigned size)
-{
-
     if (size > 1)
     {
         unsigned leftSize = size / 2;
-        int * left = (int *) malloc(size*sizeof(int));
+        int* left = malloc(size * sizeof (int));
         if (left == NULL)
+        {
+            fprintf(stderr, "Unsuccessful allocation\n");
             exit(EXIT_FAILURE);
+        }
         for (unsigned i = 0; i < size; i++)
             left[i] = arr[i];
 
@@ -167,92 +128,6 @@ void merge_sort(int * arr, unsigned size)
 
         free(left);
     }
-
 }
-
-
-void merge(int * arr, int * left, int * right,
-           unsigned leftSize, unsigned rightSize)
-{
-
-    unsigned i = 0, j = 0, k = 0;
-
-    while ((i < leftSize) && (j < rightSize))
-    {
-        if (left[i] <= right[j])
-            arr[k++] = left[i++];
-        else
-            arr[k++] = right[j++];
-    }
-
-    while (i < leftSize)
-        arr[k++] = left[i++];
-
-    while (j < rightSize)
-        arr[k++] = right[j++];
-
-}
-
-*/
-
-
-
-
-
-/* Algorithm :-
-
-   // Argument 1: Array into which the two sorted arrays are
-   //             to be merged, arr.
-   // Argument 2: First sorted array, left.
-   // Argument 3: Second sorted array, right.
-   // Argument 4: Size of the first sorted array, leftSize.
-   // Argument 5: Size of the second sorted array, rightSize.
-   // Ensure that arrSize is equal to (leftSize + rightSize).
-
-   merge() -
-
-   1. Take 3 counters, i, j and k, for arr, left and right.
-   2. Initialize i, j and k to 0.
-   3. While (j is less than leftSize) and (k is less than
-      rightSize)
-          If (left[j] is less than or equal to right[k])
-              Set arr[i] equal to left[j].
-              Increment i by 1.
-              Increment j by 1.
-          Else
-              Set arr[i] equal to right[k].
-              Increment i by 1.
-              Increment k by 1.
-      While (j is less than leftSize)
-          Set arr[i] equal to left[j].
-          Increment i by 1.
-          Increment j by 1.
-      While (k is less than rightSize)
-          Set arr[i] equal to right[k].
-          Increment i by 1.
-          Increment k by 1.
-
-
-
-   // Argument 1: Array to be sorted, arr.
-   // Argument 2: Size of the array, arrSize.
-
-   merge_sort() -
-
-   1. If (arrSize is equal to 1)
-          Do nothing, as arr is already sorted.
-      Else If (arrSize is greater than 1)
-          Create two sub-arrays left and right such that
-          leftSize is equal to (arrSize / 2) and rightSize is
-          equal to (arrSize - leftSize).
-          Sort left using merge_sort().
-          Sort right using merge_sort().
-          Merge left and right into arr using merge().
-
-
-
-   main() -
-
-   Sort any array using merge_sort().
 
 */
--- Quick-Sort/Quick-Sort.c
+++ Quick-Sort/Quick-Sort.c
@@ -1,49 +1,44 @@
-// To understand the recursion, first assume that arr is a
-// very large array. Now, prove that the quick_sort()
-// function sorts any random 1-element subset of arr. Then,
-// prove that it sorts any random 2-element subset of arr.
-// Continuing in this manner, you will notice that it sorts
-// any random n-element subset of arr.
-// For eg., when proving that the function sorts any random
-// 5-element subset of arr, you don't have to prove that it
-// sorts the two smaller subsets obtained after the pivot has
-// been sorted, as you have already proved that it sorts any
+// To understand the recursion, first assume that arr is a very large array.
+// Now, prove that the quick_sort() function sorts any random 1-element subset
+// of arr. Then, prove that it sorts any random 2-element subset of arr.
+// Continuing in this manner, you will notice that it sorts any random n-element
+// subset of arr.
+// For eg., when proving that the function sorts any random 5-element subset of
+// arr, you don't have to prove that it sorts the two smaller subsets obtained
+// after the pivot has been sorted, as you have already proved that it sorts any
 // random subset of arr with less than 5 elements.
 
 // This implementation uses Hoare's partitioning scheme.
 // Study Lomuto's partitioning scheme as well.
 
-// The recursion stops for either a 1-element array or a
-// 0-element array.
-// For a 0-element array, the upperIndex will be less than
-// the lowerIndex.
-// When the lowerIndex happens to be 0, then the upperIndex
-// will be -1.
-// This is why int indices are used in this program, instead
-// of unsigned ones.
+// The recursion stops for either a 1-element array or a 0-element array.
+// For a 0-element array, the upperIndex will be less than the lowerIndex.
+// When the lowerIndex happens to be 0, then the upperIndex will be -1.
+// This is why int indices are used in this program, instead of unsigned ones.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-
 // Unstable sort.
-void quick_sort(int *, int, int);
-int partition(int *, int, int);
-
+void quick_sort(int*, int, int);
+int partition(int*, int, int);
 
 int main(void)
 {
-
     srand((unsigned) time(NULL));
 
     int size;
     printf("Enter the no. of integers to be sorted: ");
     scanf("%d", &size);
 
-    int * arr = (int *) malloc(size * sizeof(int));
+    int* arr = malloc(size * sizeof (int));
     if (arr == NULL)
+    {
+        fprintf(stderr, "Unsuccessful allocation\n");
         exit(EXIT_FAILURE);
+    }
+
     printf("Enter %d integers: ", size);
     for (int i = 0; i < size; i++)
         scanf("%d", &arr[i]);
@@ -58,88 +53,59 @@ int main(void)
     free(arr);
 
     return EXIT_SUCCESS;
-
 }
 
-
-void quick_sort(int * arr, int lowerIndex, int upperIndex)
+void quick_sort(int* arr, int lowerIndex, int upperIndex)
 {
-
     if (lowerIndex < upperIndex)
     {
         int pivotIndexAfterSortingPivot =
             partition(arr, lowerIndex, upperIndex);
 
-        quick_sort(arr, lowerIndex,
-                   pivotIndexAfterSortingPivot - 1);
+        quick_sort(arr, lowerIndex, pivotIndexAfterSortingPivot - 1);
 
-        quick_sort(arr, pivotIndexAfterSortingPivot + 1,
-                   upperIndex);
+        quick_sort(arr, pivotIndexAfterSortingPivot + 1, upperIndex);
     }
-
 }
 
+// arr[upperIndex] should not be chosen as the pivot with Hoare's partitioning
+// scheme, as this scheme fails for a 2-element array with arr[upperIndex] as
+// the pivot.
 
-// arr[upperIndex] should not be chosen as the pivot with
-// Hoare's partitioning scheme, as this scheme fails for a
-// 2-element array with arr[upperIndex] as the pivot.
-
-int partition(int * arr, int lowerIndex, int upperIndex)
+int partition(int* arr, int lowerIndex, int upperIndex)
 {
-
     int temp;
 
-    // The chosen pivot is swapped with the first element to
-    // ensure that Hoare's scheme works.
-    // This function now works on arr[lowerIndex] instead of
-    // arr[pivotIndex].
+    int pivotIndex = lowerIndex + (rand() % (upperIndex-lowerIndex+1));
 
-    int pivotIndex =
-        lowerIndex + (rand() % (upperIndex-lowerIndex+1));
+    // The randomly chosen pivot is swapped with the first element to ensure
+    // that Hoare's scheme works, i.e. arr[lowerIndex] now becomes the pivot.
+    temp=arr[lowerIndex], arr[lowerIndex]=arr[pivotIndex], arr[pivotIndex]=temp;
 
-    temp = arr[lowerIndex];
-    arr[lowerIndex] = arr[pivotIndex];
-    arr[pivotIndex] = temp;
-
-    // If i is initialized to lowerIndex+1, then a 2-element
-    // array will not be sorted.
+    // If i is initialized to lowerIndex+1, then a 2-element array will not be
+    // sorted.
     int i = lowerIndex;
     int j = upperIndex;
 
     while (i < j)
     {
-        // If the condition is arr[i] < arr[lowerIndex]
-        // (instead of <=), then it will always fail, since
-        // arr[lowerIndex] acts as the pivot.
+        // If the condition is arr[i] < arr[lowerIndex] (instead of <=), then it
+        // will always fail, since arr[lowerIndex] acts as the pivot.
 
-        while ((i < upperIndex) &&
-                   (arr[i] <= arr[lowerIndex]))
-        {
+        while ((i < upperIndex) && (arr[i] <= arr[lowerIndex]))
             i++;
-        }
 
-        while ((j > lowerIndex) &&
-                   (arr[j] >= arr[lowerIndex]))
-        {
+        while ((j > lowerIndex) && (arr[j] >= arr[lowerIndex]))
             j--;
-        }
 
-        // At this point, it is ensured that i <= upperIndex
-        // and j >= lowerIndex.
+        // At this point, it is ensured that i <= upperIndex and
+        // j >= lowerIndex.
         if (i < j)
-        {
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+            temp = arr[i], arr[i] = arr[j], arr[j] = temp;
     }
 
-    // At this point, it is ensured that
-    // lowerIndex <= j <= upperIndex.
-    temp = arr[lowerIndex];
-    arr[lowerIndex] = arr[j];
-    arr[j] = temp;
+    // At this point, it is ensured that lowerIndex <= j <= upperIndex.
+    temp = arr[lowerIndex], arr[lowerIndex] = arr[j], arr[j] = temp;
 
     return j;
-
 }
