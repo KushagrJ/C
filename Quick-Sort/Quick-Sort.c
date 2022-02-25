1daf0a5edc248b98eda040e3341144dbee16334b
KushagrJ <kushagr.j@gmail.com>
2022-02-25 16:18:12 +0530

    Update C

--- Quick-Sort/Quick-Sort.c
+++ Quick-Sort/Quick-Sort.c
@@ -10,7 +10,6 @@
 
 // This implementation uses Hoare's partitioning scheme.
 // Study Lomuto's partitioning scheme as well.
-// Also, study another variation of Hoare's partitioning scheme given in CLRS.
 
 // The recursion stops for either a 1-element array or a 0-element array.
 // For a 0-element array, the upperIndex will be less than the lowerIndex.
@@ -20,15 +19,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
+
 
 // Unstable sort.
 void quick_sort(int*, int, int);
 int partition(int*, int, int);
 
+
 int main(void)
 {
+
     srand((unsigned) time(NULL));
 
+/*
     int size;
     printf("Enter the no. of integers to be sorted: ");
     scanf("%d", &size);
@@ -52,9 +56,42 @@ int main(void)
     printf("\n");
 
     free(arr);
+*/
+
+    int size = 10000;
+
+    int* arr = malloc(size * sizeof (int));
+    if (arr == NULL)
+    {
+        fprintf(stderr, "Unsuccessful allocation\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < size; i++)
+        arr[i] = rand();
+
+    quick_sort(arr, 0, size - 1);
+
+    bool sorting_is_successful = true;
+
+    for (int i = 0; i < (size - 1); i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            sorting_is_successful = false;
+            break;
+        }
+    }
+
+    if (sorting_is_successful)
+        printf("Congratulations\n");
+    else
+        printf("Try again\n");
 
     return EXIT_SUCCESS;
+
 }
+
 
 void quick_sort(int* arr, int lowerIndex, int upperIndex)
 {
@@ -63,12 +100,15 @@ void quick_sort(int* arr, int lowerIndex, int upperIndex)
         int pivotIndexAfterSortingPivot =
             partition(arr, lowerIndex, upperIndex);
 
-        quick_sort(arr, lowerIndex, pivotIndexAfterSortingPivot - 1);
+        // quick_sort(arr, lowerIndex, pivotIndexAfterSortingPivot - 1);
+        quick_sort(arr, lowerIndex, pivotIndexAfterSortingPivot);
 
         quick_sort(arr, pivotIndexAfterSortingPivot + 1, upperIndex);
     }
 }
 
+
+/*
 // arr[upperIndex] should not be chosen as the pivot with Hoare's partitioning
 // scheme, as this scheme fails for a 2-element array with arr[upperIndex] as
 // the pivot.
@@ -109,4 +149,43 @@ int partition(int* arr, int lowerIndex, int upperIndex)
     temp = arr[lowerIndex], arr[lowerIndex] = arr[j], arr[j] = temp;
 
     return j;
+}
+*/
+
+
+// This is another variation of Hoare's partitioning scheme in which the pivot
+// doesn't reach its final position after partitioning.
+// Instead, the subarray gets partitioned with respect to the pivot and the
+// pivot gets shifted to either of the two partitions.
+// Thus, the call to quick_sort() is quick_sort(arr, l, p), instead of
+// quick_sort(arr, l, p - 1).
+
+int partition(int* arr, int lower_index, int upper_index)
+{
+    int temp;
+
+    int pivot_index = lower_index + (rand() % (upper_index - lower_index + 1));
+
+    temp = arr[lower_index];
+    arr[lower_index] = arr[pivot_index];
+    arr[pivot_index] = temp;
+
+    int pivot = arr[lower_index];
+
+    int i = lower_index;
+    int j = upper_index;
+
+    while (1)
+    {
+        while ((i < upper_index) && (arr[i] < pivot))
+            i++;
+
+        while ((j > lower_index) && (arr[j] > pivot))
+            j--;
+
+        if (i < j)
+            temp = arr[i], arr[i] = arr[j], arr[j] = temp;
+        else
+            return j;
+    }
 }
