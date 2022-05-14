3e2987abad1c8517a68d18e1482a13e4b68fa92d
KushagrJ <kushagr.j@gmail.com>
2022-05-14 19:20:28 +0530

    Update C

--- Greedy/03-Kruskal-Algorithm/Kruskal-Algorithm.c
+++ Greedy/03-Kruskal-Algorithm/Kruskal-Algorithm.c
@@ -1,6 +1,6 @@
 // To be tested properly.
 
-// A disjoint set forest has been used for the vertices.
+// A disjoint-set forest has been used for the vertices.
 
 // Incorporate 'Union by Rank' and 'Path Compression' before posting on
 // Code Review Stack Exchange.
@@ -324,3 +324,109 @@ void free_graph(Graph* ptr_g)
     }
 
 }
+
+
+
+
+
+/*
+
+ * The Kruskal's Algorithm (The Minimum Spanning Tree Problem) :-
+
+   Electronic circuit designs, such as the connections of logic gates on a
+   breadboard, often need to make the pins of several components electrically
+   equivalent by wiring them together.
+   To interconnect a set of n pins, the designer can use an arrangement of
+   (n - 1) wires, each connecting two pins.
+   Of all such arrangements, the one that uses the least amount of wire is
+   usually the most desirable.
+
+   This problem can be modelled using a weighted, connected and undirected graph
+   G = (V, E), where V is the set of pins, E is the set of all possible
+   interconnections between pairs of pins, and for each edge (u, v) belonging to
+   E, w(u, v) specifies the cost (amount of wire needed) to connect u and v.
+   The goal is to find a minimum spanning tree of this graph, i.e. a spanning
+   tree whose total weight is minimum among all possible spanning trees.
+
+   Given a weighted, connected and undirected graph G = (V, E) having no self
+   loops and no parallel edges, the Kruskal's algorithm produces a minimum
+   spanning tree of G.
+   [For a weighted, complete and undirected graph G, there are (|V| ^ (|V| - 2))
+    different possible spanning trees]
+
+   The optimal substructure property of this problem is as follows:
+   Suppose that after running some algorithm, it is found that a minimum
+   spanning tree of G is T.
+   Then, for every subtree T' of T, T' must be a minimum spanning tree of the
+   corresponding subgraph G' of G consisting of those vertices present in T' and
+   all edges of G between those vertices.
+
+   If this problem is solved using dynamic programming, then the overlapping
+   subproblems property of this problem is as follows :-
+   Every dynamic programming algorithm checking all possible spanning trees
+   will have to examine the same subtrees when trying to build different
+   spanning trees multiple times.
+   Solving the minimum spanning tree problem using dynamic programming is an
+   overkill, as the greedy approach results in much more efficient algorithms.
+
+
+   In the Kruskal's algorithm, a disjoint-set data structure (efficiently
+   implemented as a disjoint-set forest with the union-by-rank and the
+   path-compression heuristics) is maintained whose disjoint sets consist of the
+   vertices belonging to the given graph.
+   At the beginning, there are |V| disjoint sets, such that every set consists
+   of a single vertex.
+   In every iteration, a lowest-weight edge between two vertices belonging to
+   different sets is added to the set A of edges constituting an MST.
+   Consequently, the two sets containing the two vertices are combined to form a
+   a new set, with the previous two sets getting destroyed.
+   After all edges have been processed (or after (|V| - 1) edges have been added
+   to A), the algorithm stops and A consists of the edges forming an MST.
+   [The disjoint sets of vertices should be thought of as separate, smaller
+    trees consisting of the corresponding vertices.
+    Thus, if in any iteration a lowest-weight edge between two vertices
+    belonging to the same set is found, then that edge is skipped, as adding
+    that edge to A would make the graph formed by the edges of A cyclic.]
+
+   In the Kruskal's algorithm, after every iteration, the edges selected so far
+   may or may not form a tree.
+
+
+   The operation MAKE-SET(v) creates a new set consisting only of vertex v and
+   makes v the representative vertex of that set.
+   The operation FIND-SET(v) returns the representative vertex of the set
+   containing vertex v.
+   The operation UNION(u, v) combines the two sets containing vertex u and
+   vertex v, respectively, to form a new set, makes some vertex in that set its
+   representative, and destroys the previous two sets.
+
+
+   MST-KRUSKAL(G, w) --------------------------------------------> O(|E| log|V|)
+
+   01. A = phi (empty set)
+   02. for each vertex v belonging to G.V
+   03.     MAKE-SET(v)
+   04. create a list of the edges belonging to G.E
+   05. sort the list of the edges in non-decreasing order by weight w
+   06. for each edge (u, v) taken from the sorted list in order
+   07.     if FIND-SET(u) != FIND-SET(v)
+   08.         add (u, v) to A
+   09.         UNION(u, v)
+   10. return A
+
+   Assuming a disjoint-set forest implementation,
+   (1) Initializing the set A in line 01 takes O(1) time.
+   (2) Creating the list in line 04 takes either O(1) or O(|E|) time.
+   (3) Sorting the list in line 05 takes O(|E| log|E|) time.
+   (4) All MAKE-SET, FIND-SET and UNION operations from lines 02-03 and 06-09
+       together take O((|V| + |E|) alpha(|V|)) time, where alpha is a very
+       slowly growing function due to the heuristics.
+   Since G is connected, therefore |E| >= (|V| - 1), and so the disjoint-set
+   operations take O(|E| alpha(|V|)) time.
+   Moreover, since alpha(|V|) = O(log |V|) = O(log |E|), the total running time
+   of the Kruskal's algorithm becomes O(|E| log|E|).
+   Since the graph doesn't contain any self loops and parallel edges, therefore
+   |E| < (|V| ^ 2), making log|E| = O(log|V|).
+   So, the running time can be restated as O(|E| log|V|).
+
+ */
