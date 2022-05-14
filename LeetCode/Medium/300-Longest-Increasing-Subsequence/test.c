a82f883570af6740cb80e6a4ce7daa372c9bfc31
KushagrJ <kushagr.j@gmail.com>
2022-05-14 23:38:29 +0530

    Update C

---
+++ LeetCode/Medium/300-Longest-Increasing-Subsequence/test.c
@@ -0,0 +1,36 @@
+// ans[i] is the length of a longest increasing subsequence in the range
+// 0-th integer to i-th integer, under the constraint that nums[i] is present in
+// the subsequence.
+
+int length_of_longest_increasing_subsequence(int* nums, int nums_size)
+{
+
+    int* ans = malloc(nums_size * sizeof (int));
+    if (ans == NULL)
+    {
+        fprintf(stderr, "Unsuccessful allocation\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ans[0] = 1;
+    int max_length = 1;
+
+    for (int i = 1; i < nums_size; i++)
+    {
+        ans[i] = 1;
+
+        for (int j = (i - 1); j >= 0; j--)
+        {
+            if ((nums[i] > nums[j]) && (ans[i] < (ans[j] + 1)))
+                ans[i] = (ans[j] + 1);
+        }
+
+        if (max_length < ans[i])
+            max_length = (ans[i]);
+    }
+
+    free(ans);
+
+    return max_length;
+
+}
